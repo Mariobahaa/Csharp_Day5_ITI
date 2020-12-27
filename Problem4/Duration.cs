6d9ee328e70eac94a440ba47e0e9f4cccf8c726f
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-27 23:37:39 +0200

    Dunction Overrides Completed

---
+++ Problem4/Duration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem4
+{
+    class Duration
+    {
+        int hours;
+        int minutes;
+        int seconds;
+
+        public Duration(int _h, int _m, int _s)
+        {
+            hours = _h;
+            minutes = _m;
+            seconds = _s;
+        }
+
+        public Duration(int time)
+        {
+            int t = time;
+            hours = t / (60 * 60);
+            t %= (60 * 60);
+            minutes = t / (60);
+            t %= 60;
+            seconds = t;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Duration dur = (Duration)obj;
+            return (dur.hours == hours) && (dur.minutes == minutes) && (dur.seconds == minutes);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            String str;
+            if(hours>0)
+            {
+                str = String.Format($"Hours: {hours}, Minutes: {minutes}, Seconds: {seconds}");
+            }
+            else
+            {
+                if(minutes>0)
+                {
+                    str= String.Format($"Minutes: {minutes}, Seconds: {seconds}");
+                }
+                else
+                {
+                    if (seconds > 0)
+                    {
+                        str = String.Format($"Seconds: {seconds}");
+
+                    }
+                    else
+                    {
+                        str = String.Format($"Seconds: "+0);
+                    }
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ Problem4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duration d1 = new Duration(7800);
+            // Console.WriteLine($"{d1.hours} {d1.minutes} {d1.seconds}");
+           // Console.WriteLine(d1.ToString());
+        }
+    }
+}
