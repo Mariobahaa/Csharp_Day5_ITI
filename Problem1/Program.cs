aee04dd3ef809408b863cfe5a31d7b68297a3fb0
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-27 20:31:55 +0200

    Problem 1 (1-6) Done

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Problem1
 {
@@ -6,7 +10,58 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
-            Console.Write(new Point3D(10, 10));
+            Console.WriteLine(new Point3D(10, 10,10));
+
+            bool xparsed= false, yparsed = false,  zparsed = false;
+
+            Point3D P1 = new Point3D();
+            Point3D P2 = new Point3D();
+
+            for(int i=0;i<2;i++)
+            {
+                int _X, _Y, _Z;
+                do
+                {
+                    xparsed = int.TryParse(Console.ReadLine(), out _X);
+
+                } while (!xparsed);
+
+                do
+                {
+                    yparsed = int.TryParse(Console.ReadLine(), out _Y);
+
+                } while (!yparsed);
+
+                do
+                {
+                    zparsed = int.TryParse(Console.ReadLine(), out _Z);
+
+                } while (!zparsed);
+            if(i==0)
+                P1 = new Point3D(_X, _Y, _Z);
+            else if(i==1)
+                P2 = new Point3D(_X, _Y, _Z);
+            }
+
+            if (P1 == P2) Console.WriteLine("hello");
+
+            Console.WriteLine(P1.ToString());
+
+            Point3D[] PArr = { new Point3D(1, 1, 3), new Point3D(1, 2, 3), new Point3D(2, 1, 3), new Point3D(3, 1, 3) };
+
+            Array.Sort(PArr, new PointCompare());
+
+            for(int i = 0; i < PArr?.Length; i++)
+            {
+                Console.WriteLine(PArr[i]);
+            }
+
+            Point3D P3 = (Point3D) P1.Clone();
+            Console.WriteLine(P3.CompareTo(P1));
+
+
         }
+
+
     }
 }
