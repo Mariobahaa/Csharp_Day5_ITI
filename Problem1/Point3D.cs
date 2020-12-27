1ecd94b5936129de4b43c8e3a36736509058899c
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-27 13:58:26 +0200

    Initial commit

---
+++ Problem1/Point3D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem1
+{
+    class Point3D
+    {
+        int x, y, z;
+
+        public Point3D(int _X, int _Y, int _Z)
+        {
+            x = _X;
+            y = _Y;
+            z = _Z;
+        }
+
+        public Point3D(int _X, int _Y) : this(_X,_Y,0)
+        {
+            ;
+        }
+
+        public Point3D(int _X) : this(_X,0, 0)
+        {
+            ;
+        }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({x}, {y}, {z})";
+        }
+    }
+}
---
+++ Problem1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new Point3D(10, 10));
+        }
+    }
+}
