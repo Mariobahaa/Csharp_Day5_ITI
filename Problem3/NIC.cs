0cbad0092e144c2e4aeaf68f3de306e53b1c7aa0
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-27 21:47:23 +0200

    Problem3 Completed (9)

---
+++ Problem3/NIC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem3
+{
+    class NIC
+    {
+        String Manufacture;
+        int MACaddress;
+        String Type;
+
+        public static NIC NICcard
+        {
+            get;
+        } = new NIC("IEEE", 111, "Ethernet");
+
+
+        private NIC(String m, int ad , String t)
+        {
+            Manufacture = m;
+            MACaddress = ad;
+            Type = t;
+        }
+
+    }
+}
---
+++ Problem3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NIC n1 = NIC.NICcard;
+            
+        }
+    }
+}
